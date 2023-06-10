3873931d6be62957d90ec54fcce1c3f78195ec44
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-06-10 13:07:43 +0300

    updated / fixed log path in main

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	err := server.AaaawwwwwSheeeetttttItsAboutToGoDown("http")
+	err := server.AaaawwwwwSheeeetttttItsAboutToGoDown("http", server.LOG_PATH)
 	if err != nil {
 		fmt.Printf("Error: %s", err) // Keep this line during development, for debugging using terminal
 		log.Printf("Error: %s", err)
