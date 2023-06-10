d711224c59f2d864e4611ef981da986b0cdc96ae
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-06-10 12:26:59 +0300

    initial unit test for initiateLogging()

---
+++ backend/server/server_test.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"bufio"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestInitiateLogging(t *testing.T) {
+	// Call the initiateLogging function
+	err := initiateLogging()
+	if err != nil {
+		t.Fatalf("initiateLogging() failed: %v", err)
+	}
+
+	// List all files in the LOG_PATH directory
+	files, err := ioutil.ReadDir(LOG_PATH)
+	if err != nil {
+		t.Fatalf("Error reading log directory: %v", err)
+	}
+
+	// Search for the log file created by initiateLogging
+	var logFilePath string
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), "log_") {
+			logFilePath = LOG_PATH + file.Name()
+			break
+		}
+	}
+
+	if logFilePath == "" {
+		t.Fatal("Log file not created")
+	}
+
+	// Test logging by writing a log message
+	logMessage := "Test log message"
+	log.Print(logMessage)
+
+	// Read the content of the log file
+	logFile, err := os.Open(logFilePath)
+	if err != nil {
+		t.Fatalf("Error opening log file: %v", err)
+	}
+	defer logFile.Close()
+
+	scanner := bufio.NewScanner(logFile)
+	var logFileContent string
+	for scanner.Scan() {
+		logFileContent += scanner.Text()
+	}
+
+	// Check if the log message is present in the log file
+	if !strings.Contains(logFileContent, logMessage) {
+		t.Fatal("Log message not written to log file")
+	}
+}
