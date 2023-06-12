b97ad3cec9fbd4eb3b3390efd96e8c64fae5b209
mainm0e <mainm0e1996@gmail.com>
2023-06-12 15:11:30 +0300

    adi working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import LoginPage from "./LoginPage/LoginPage";
 import RegisterPage from "./RegisterPage/RegisterPage";
 import MainPage from "./MainPage/MainPage";
 import ErrorPage from "./ErrorPage/ErrorPage";
-import MyComponent from "./test_componen";
 import './App.css';
 function App() {
   // function handle that check url and return the page
@@ -22,7 +21,7 @@ function App() {
 
   return (
     <div className="App">
-      <MyComponent/>
+      {getPage()}
     </div>
   );
 };
