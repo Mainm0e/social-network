489182229b0f7c8042a0b14fa7faa441e65189c2
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-06-17 12:23:53 +0300

    sockets NewClient() function

--- backend/sockets/sockets.go
+++ backend/sockets/sockets.go
@@ -1,5 +1,7 @@
 package sockets
 
+import "github.com/gorilla/websocket"
+
 /*
 NewManager is a function that creates a new Manager. This function doesn't take any
 parameters and returns a pointer to a newly created Manager. It initializes all
@@ -15,5 +17,22 @@ func NewManager() *Manager {
 		Unregister: make(chan *Client),
 		Clients:    ClientList{},
 		Handlers:   make(map[string]EventHandler),
+	}
+}
+
+/*
+NewClient is a function that creates a new Client. It takes two parameters:
+a pointer to a websocket.Conn (which represents the WebSocket connection
+between the server and the client) and a pointer to a Manager (which manages
+the client and other clients). The function returns a pointer to a newly
+created Client.  This function is typically called after a new WebSocket
+connection has been established and a new Client needs to be created to
+manage the connection.
+*/
+func NewClient(conn *websocket.Conn, wsManager *Manager) *Client {
+	return &Client{
+		Connection: conn,
+		Manager:    wsManager,
+		Egress:     make(chan []byte),
 	}
 }
