02aba2f1ba367ca23e221b2b7b12110817a07ae7
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-07-05 00:07:15 +0300

    addition of chat interface for sockets

--- backend/sockets/socket_chat_structs.go
+++ backend/sockets/socket_chat_structs.go
@@ -32,3 +32,44 @@ type IsTyping struct {
 	ClientUsername string `json:"clientUsername"` // Username of the clientthat is typing
 	TargetName     string `json:"targetName"`     // Username / GroupTitle to identify chat in which typing is happening
 }
+
+// ChatMsg is an interface that is implemented by both PrivateMsg and GroupMsg.
+// This allows us to use the same functions for both types of messages.
+type ChatMsg interface {
+	GetSender() string
+	GetReceiver() string
+	GetMessage() string
+	GetTimestamp() string
+}
+
+func (p *PrivateMsg) GetSender() string {
+	return p.SenderUsername
+}
+
+func (p *PrivateMsg) GetReceiver() string {
+	return p.ReceiverUsername
+}
+
+func (p *PrivateMsg) GetMessage() string {
+	return p.Message
+}
+
+func (p *PrivateMsg) GetTimestamp() string {
+	return p.Timestamp
+}
+
+func (g *GroupMsg) GetSender() string {
+	return g.SenderUsername
+}
+
+func (g *GroupMsg) GetReceiver() string {
+	return g.GroupTitle
+}
+
+func (g *GroupMsg) GetMessage() string {
+	return g.Message
+}
+
+func (g *GroupMsg) GetTimestamp() string {
+	return g.Timestamp
+}
