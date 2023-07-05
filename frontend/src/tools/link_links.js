6894eb0b69fa252131855c6e5cea825f2c7c2259
Mainm0e <mainm0e1996@gmail.com>
2023-07-05 14:17:36 +0300

    frontend update

---
+++ frontend/src/tools/link_links.js
@@ -0,0 +1,31 @@
+export const your_link ={
+    "Home": '/',
+    "Create Post": '#createpost',
+    "Postlists": '#postlist',
+    "create group": '#creategroup',
+    "Explore users": "/user",
+    "Explore groups": "/group",
+}
+export const profile_link ={
+    "Home": '/',
+    "Postlists": '#postlist',
+    "Explore user": "/user",
+    "Explore groups": "/group",
+}
+
+export const group_link ={
+    "Home": '/',
+    "Postlists": '#postlist',
+    "Create Post": '#createpost',
+    "Events": "#eventlist",
+    "Create event": '#createevent',
+    "Explore user": "/user",
+    "Explore groups": "/group",
+}
+
+export const default_link ={
+    "Home": '/',
+    "Explore user": "/user",
+    "Explore groups": "/group",
+}
+
