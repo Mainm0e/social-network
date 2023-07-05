663fbc52699bf205726b07bdd3ce98878eacd8ea
soma <23520374+SomaSapien@users.noreply.github.com>
2023-07-05 18:51:41 +0300

    reordering of structs

--- backend/sockets/socket_chat_structs.go
+++ backend/sockets/socket_chat_structs.go
@@ -1,24 +1,37 @@
 package sockets
 
+import "time"
+
+/************************** COMMON EVENT STRUCTS **************************/
 type PrivateMsg struct {
 	SenderID   int    `json:"senderID"`
 	ReceiverID int    `json:"receiverID"`
 	Message    string `json:"message"`
-	Timestamp  string `json:"timeStamp"`
+	Timestamp  string `json:"timeStamp"` // Received as an empty string from frontend
 }
 
 type GroupMsg struct {
 	SenderID   int    `json:"senderID"`
 	ReceiverID int    `json:"receiverID"`
 	Message    string `json:"message"`
-	Timestamp  string `json:"timeStamp"`
+	Timestamp  string `json:"timeStamp"` // Received as an empty string from frontend
 }
+
+type IsTyping struct {
+	ChatType string `json:"chatType"`
+	ClientID int    `json:"clientID"` // UserID of the client that is typing
+	TargetID int    `json:"targetID"` // UserID / GroupID to identify chat in which typing is happening
+}
+
+/*********************** ONLY FROM FRONTEND EVENT STRUCT ******************/
 
 type ChatHistoryRequest struct {
 	ChatType string `json:"chatType"`
 	ClientID int    `json:"clientID"`
 	TargetID int    `json:"targetID"` // UserID or GroupID
 }
+
+/*********************** ONLY FROM BACKEND EVENT STRUCT ******************/
 
 type ChatHistory struct {
 	ChatType    string   `json:"chatType"`
@@ -27,11 +40,8 @@ type ChatHistory struct {
 	ChatHistory []string `json:"chatHistory"`
 }
 
-type IsTyping struct {
-	ChatType string `json:"chatType"`
-	ClientID int    `json:"clientID"` // UserID of the client that is typing
-	TargetID int    `json:"targetID"` // UserID / GroupID to identify chat in which typing is happening
-}
+/*********************** INTERFACES & METHODS ****************************/
+/********* NOTE TO SELF: NEVER FORGET HOW COOL INTERFACES ARE! ***********/
 
 // ChatMsg is an interface that is implemented by both PrivateMsg and GroupMsg.
 // This allows us to use the same functions for both types of messages.
@@ -41,46 +51,124 @@ type ChatMsg interface {
 	GetMessage() string
 	GetTimestamp() string
 	GetMsgType() string
+
+	// Define fields that are common to both PrivateMsg and GroupMsg
+	WrapMsg() struct {
+		Type    string `json:"type"`
+		Payload struct {
+			SenderID   int    `json:"senderID"`
+			ReceiverID int    `json:"receiverID"`
+			Message    string `json:"message"`
+			Timestamp  string `json:"timeStamp"`
+		} `json:"payload"`
+	}
 }
 
-/*********** NOTE TO SELF: NEVER FORGET HOW COOL INTERFACES ARE! ************/
-
+/*
+GetSenderID() returns the senderID of the message. This is a common field for
+both PrivateMsg and GroupMsg. This is useful when we want to send a message to
+the frontend, but we don't know if it is a PrivateMsg or GroupMsg.
+*/
 func (p *PrivateMsg) GetSenderID() int {
 	return p.SenderID
-}
-
-func (p *PrivateMsg) GetReceiverID() int {
-	return p.ReceiverID
-}
-
-func (p *PrivateMsg) GetMessage() string {
-	return p.Message
-}
-
-func (p *PrivateMsg) GetTimestamp() string {
-	return p.Timestamp
 }
 
 func (g *GroupMsg) GetSenderID() int {
 	return g.SenderID
 }
 
+/*
+GetReceiverID() returns the receiverID of the message. This is a common field for
+both PrivateMsg and GroupMsg. This is useful when we want to send a message to
+the frontend, but we don't know if it is a PrivateMsg or GroupMsg.
+*/
+func (p *PrivateMsg) GetReceiverID() int {
+	return p.ReceiverID
+}
+
 func (g *GroupMsg) GetReceiverID() int {
 	return g.ReceiverID
+}
+
+/*
+GetMessage() returns the message string. This is a common field for both
+PrivateMsg and GroupMsg. This is useful when we want to send a message to
+the frontend, but we don't know if it is a PrivateMsg or GroupMsg.
+*/
+func (p *PrivateMsg) GetMessage() string {
+	return p.Message
 }
 
 func (g *GroupMsg) GetMessage() string {
 	return g.Message
 }
 
-func (g *GroupMsg) GetTimestamp() string {
-	return g.Timestamp
+/*
+GetTimestamp() returns the timestamp of the message. This is a common field for
+both PrivateMsg and GroupMsg. This is useful when we want to send a message to
+the frontend, but we don't know if it is a PrivateMsg or GroupMsg.
+*/
+func (p *PrivateMsg) GetTimestamp() string {
+	return time.Now().Format("2006-01-02 15:04:05")
 }
 
+func (g *GroupMsg) GetTimestamp() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+/*
+GetMsgType() returns a string that identifies the type of message, and is a
+common field for both PrivateMsg and GroupMsg. This is useful when we want to
+send a message to the frontend, but we don't know if it is a PrivateMsg or
+GroupMsg.
+*/
 func (p *PrivateMsg) GetMsgType() string {
 	return "PrivateMsg"
 }
 
 func (g *GroupMsg) GetMsgType() string {
 	return "GroupMsg"
+}
+
+/*
+WrapMsg() returns a struct that contains the fields that are common to both
+PrivateMsg and GroupMsg. This is useful when we want to send a message to
+the frontend, but we don't know if it is a PrivateMsg or GroupMsg.
+*/
+func (p *PrivateMsg) WrapMsg() struct {
+	SenderID   int    `json:"senderID"`
+	ReceiverID int    `json:"receiverID"`
+	Message    string `json:"message"`
+	Timestamp  string `json:"timeStamp"`
+} {
+	return struct {
+		SenderID   int    `json:"senderID"`
+		ReceiverID int    `json:"receiverID"`
+		Message    string `json:"message"`
+		Timestamp  string `json:"timeStamp"`
+	}{
+		SenderID:   p.SenderID,
+		ReceiverID: p.ReceiverID,
+		Message:    p.Message,
+		Timestamp:  p.GetTimestamp(),
+	}
+}
+
+func (g *GroupMsg) WrapMsg() struct {
+	SenderID   int    `json:"senderID"`
+	ReceiverID int    `json:"receiverID"`
+	Message    string `json:"message"`
+	Timestamp  string `json:"timeStamp"`
+} {
+	return struct {
+		SenderID   int    `json:"senderID"`
+		ReceiverID int    `json:"receiverID"`
+		Message    string `json:"message"`
+		Timestamp  string `json:"timeStamp"`
+	}{
+		SenderID:   g.SenderID,
+		ReceiverID: g.ReceiverID,
+		Message:    g.Message,
+		Timestamp:  g.GetTimestamp(),
+	}
 }
