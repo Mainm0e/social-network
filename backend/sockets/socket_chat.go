9947e5e5d24e6ddf125c594863e174fb4bc0f1cc
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-07-05 11:33:37 +0300

    BroadcastPrivateMsg() method

--- backend/sockets/socket_chat.go
+++ backend/sockets/socket_chat.go
@@ -1,6 +1,9 @@
 package sockets
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 /********************** PRIVATE MESSAGE LOGIC *******************************/
 
@@ -17,6 +20,42 @@ func UnmarshalJSONToPrivateMsg(jsonMsg []byte) (*PrivateMsg, error) {
 	}
 	return &privateMsg, nil
 }
+
+/*
+BroadcastProvateMessage() is a method of the Manager struct, which takes a
+receiver ID and a json byte array as parameters. It then broadcasts the json
+byte array to the the client with the given receiver ID. It returns an error
+value, which is non-nil if any of the broadcasting operations failed or if
+the receiver was not found.
+*/
+func (m *Manager) BroadcastPrivateMsg(receiver string, payloadJSON []byte) error {
+	var sent bool
+	// The range function on a sync.map accepts a function of the form
+	// func(key, value interface{}) bool, which it calls once for each
+	// item in the map. If the function returns false, the iteration stops.
+	m.Clients.Range(func(key, client interface{}) bool {
+		if client.(*Client).ID == receiver {
+			select {
+			case client.(*Client).Egress <- payloadJSON:
+				sent = true
+			default:
+				close(client.(*Client).Egress)
+				m.Clients.Delete(key)
+				return false
+			}
+			return false // Stop iteration after the message has been sent to target client
+		}
+		return true
+	})
+
+	if !sent {
+		return errors.New("message could not be sent: receiver not found")
+	}
+
+	return nil
+}
+
+/********************** GROUP MESSAGE LOGIC **********************************/
 
 /********************** COMMON LOGIC / FUNCTIONS *****************************/
 
@@ -42,25 +81,6 @@ GroupMsg, and broadcasts it to all clients in the chat. It returns an error valu
 which is non-nil if any of the broadcasting operations failed.
 */
 func (m *Manager) BroadcastMessage(msg ChatMsg) error {
-	receiver := msg.GetReceiver()
-	sender := msg.GetSender()
-	message := msg.GetMessage()
-	timestamp := msg.GetTimestamp()
-	msgType := msg.GetType()
-
-	// payload to be sent to clients
-	payload := map[string]interface{}{
-		"type": msgType,
-		"payload": map[string]string{
-			"senderUsername": sender,
-			"receiver":       receiver,
-			"message":        message,
-			"timeStamp":      timestamp,
-		},
-	}
-
-	// TODO: Logic to broadcast message to all clients in the chat
-
 	return nil
 }
 
