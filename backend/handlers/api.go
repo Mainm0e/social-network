bf49c77147426ec26ce44a79af0484b705358568
soma <23520374+SomaSapien@users.noreply.github.com>
2023-07-06 15:18:43 +0300

    removed unnecessary logging

--- backend/handlers/api.go
+++ backend/handlers/api.go
@@ -17,7 +17,7 @@ func HTTPEventRouter(w http.ResponseWriter, r *http.Request) {
 		var event events.Event
 		err := json.NewDecoder(r.Body).Decode(&event)
 		r.Body.Close() // Must close the body after decoding it to free up resources
-		log.Println("Event:", event.Type, "payload: ", string(event.Payload))
+		// log.Println("Event:", event.Type, "payload: ", string(event.Payload))
 		if err != nil {
 			log.Println("Error decoding event:", err)
 			response := Response{"Error decoding event:" + err.Error(), events.Event{}, http.StatusBadRequest}
