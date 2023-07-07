6090c024f3ef1a9a25d85d8a190c873238d85e04
soma <23520374+SomaSapien@users.noreply.github.com>
2023-07-07 18:18:34 +0300

    Added WebSocketContect to frontend

---
+++ frontend/src/WebSocketContext/websocketcontext.js
@@ -0,0 +1,37 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+// Create a context that will hold the websocket connection
+export const WebSocketContext = createContext(null);
+
+// Create a provider component that will wrap other components and provide them with the websocket connection
+export const WebSocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // On component mount, try to connect to the websocket server
+    const ws = new WebSocket('ws://localhost:8080/ws');
+
+    ws.onopen = () => {
+      console.log('connected to ws server');
+      setSocket(ws);
+    };
+
+    ws.onerror = (error) => {
+      console.log('failed to connect to ws server:', error);
+    };
+
+    // On component unmount, close the websocket connection
+    return () => {
+      if (ws) {
+        ws.close();
+      }
+    };
+  }, []);
+
+  return (
+    // Provide the websocket connection to child components
+    <WebSocketContext.Provider value={socket}>
+      {children}
+    </WebSocketContext.Provider>
+  );
+};
