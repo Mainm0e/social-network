77c3b4d9dacceb199061ab6a1658d195ca4005f8
Steve <23520374+SomaSapien@users.noreply.github.com>
2023-07-11 15:26:00 +0300

    New PackageErrorEvent() function

---
+++ backend/events/events.go
@@ -0,0 +1,24 @@
+package events
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+PackageErrorEvent takes an ErrorMessage struct as input and returns a pointer to an
+Event struct, as well as an error value, which is non-nil if there was an error encountered
+during the marshalling of the ErrorMessage struct. It is used to package an ErrorMessage
+struct into an Event struct, which can then be converted to a JSON byte slice and sent
+to the frontend / client.
+*/
+func PackageErrorEvent(errMsg ErrorMessage) (*Event, error) {
+	errMsgBytes, err := json.Marshal(errMsg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal ErrorMessage: %v", err)
+	}
+	return &Event{
+		Type:    "error",
+		Payload: errMsgBytes,
+	}, nil
+}
