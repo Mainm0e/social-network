0b379f256e9ee6acc116d81d012d375bedee6762
mainm0e <mainm0e1996@gmail.com>
2023-07-12 16:38:17 +0300

    event event event event done

--- frontend/src/Common/Event/EventList.jsx
+++ frontend/src/Common/Event/EventList.jsx
@@ -1,74 +1,100 @@
-import { useEffect } from "react"
-import { fetchData } from "../../tools/fetchData"
-import { getCookie, getUserId } from "../../tools/cookie"
-import { useState } from "react"
+import { useEffect } from "react";
+import { fetchData } from "../../tools/fetchData";
+import { getCookie, getUserId } from "../../tools/cookie";
+import { useState } from "react";
 
 const EventList = ({ clearBox }) => {
-    const url = new URL(window.location.href)
-    const searchParams = new URLSearchParams(url.search)
-    const id = searchParams.get("id")
-    const [event, setEvent] = useState([])
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+  const id = searchParams.get("id");
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const method = "POST";
+    const type = "getGroupEvents";
+    const payload = {
+      sessionId: getCookie("sessionId"),
+      groupId: parseInt(id),
+    };
+    fetchData(method, type, payload).then((data) => {
+      setData(data.events);
+    });
+    console.log("aösldkfjasödlkfjaösldkfj",data);
+  }, [id]);
+  const acceptEvent = (event,option) => {
+    const method = "POST";
+    const type = "participate";
+    const payload = {
+      sessionId: getCookie("sessionId"),
+      groupId: event.groupId,
+      eventId: event.eventId,
+      memberId: getUserId("userId"),
+      option: option,
+    };
+    fetchData(method, type, payload).then((data) => {
+       console.log("data", payload); 
+      console.log(data);
+    });
+  };
 
-    useEffect(() => {
-        const method = "POST"
-        const type = "getGroupEvents"
-        const payload = { sessionId: getCookie("sessionId"), groupId: parseInt(id) }
-        fetchData(method, type, payload).then((data) => {
-            console.log("EventList",payload)
-            setEvent(data)
-        }
-        )
-    }, [id])
-        const acceptEvent = (event) => {
-            const method = "POST"
-            const type = "participate"
-            const payload = { sessionId: getCookie("sessionId"), eventId: event.id,memberId:getUserId("userId"),option:event }
-            fetchData(method, type, payload).then((data) => {
-                console.log(data)
-            })
-        }
-
-        const renderNotifications = () => {
-            return (
-                <>
-                {event.events.map((event, index) => (
-                    <div className="event-list-item" key={index}>
-                      <div className="event-list-item-header">
-                        <h2>{event.event.title}</h2>
-                      </div>
-                      <div className="event-list-item-body">
-                        <p>{event.event.content}</p>
-                        {/* creater */}
-                        <div className="event-creater">
-                            <p>Created by: {event.creatorProfile.firstName}</p>
-                        </div>
-                      </div>
-                      <div className="event-list-item-footer">
-                        <p>{event.event.date}</p>
-                      </div>
-                        <div className="event-list-item-footer">
-                            <button onClick={() => acceptEvent("going")}>Going</button>
-                            <button onClick={() => acceptEvent("not_going")}>Not going</button>
-                        </div>
-                    </div>
-                ))}
-                </>
-            )
-        };
+  const renderNotifications = () => {
+    console.log("data", data);
     return (
-        <div className="event-list">
+      <>
+        {data.map((event, index) => (
+          <div className="event-list-item" key={index}>
+            <div className="event-list-item-header">
+              <h2>{event.event.title}</h2>
+            </div>
+            <div className="event-list-item-body">
+              <p>{event.event.content}</p>
+              <div className="event-creater">
+                <p>Created by: {event.creatorProfile.firstName}</p>
+              </div>
+            </div>
+            <div className="event-list-item-footer">
+              <p>{event.event.date}</p>
+            </div>
+            <div className="event-list-item-footer">
+              <button onClick={() => acceptEvent(event.event,"going")}>Going</button>
+              <button onClick={() => acceptEvent(event.event,"not_going")}>
+                Not going
+              </button>
+            </div>
+          </div>
+        ))}
+      </>
+    ); 
+  };
+  if (data !== undefined) {
+  return (
+    <div className="event-list">
+      <div className="event-list-header">
+        <h1>Events</h1>
+      </div>
+      <div className="event-list-body">
+       {renderNotifications()}  
+      </div>
+      <div className="event-list-footer">
+        <button onClick={clearBox}>Close</button>
+      </div>
+    </div>
+  );
+  } else if (data === []){
+    return (
+      <div className="event-list">
         <div className="event-list-header">
           <h1>Events</h1>
         </div>
         <div className="event-list-body">
-            {renderNotifications()}
+          <p>No events</p>
         </div>
         <div className="event-list-footer">
           <button onClick={clearBox}>Close</button>
         </div>
       </div>
-    )
-}
+    );
+  }
+};
 
-export default EventList
+export default EventList;
