149f76b90b1a621629ff4118e24a696207b7ed92
mainm0e <mainm0e1996@gmail.com>
2023-07-12 15:11:49 +0300

    check

--- frontend/src/Common/Event/EventList.jsx
+++ frontend/src/Common/Event/EventList.jsx
@@ -1,27 +1,73 @@
 import { useEffect } from "react"
 import { fetchData } from "../../tools/fetchData"
 import { getCookie } from "../../tools/cookie"
+import { useState } from "react"
 
 const EventList = ({ clearBox }) => {
     const url = new URL(window.location.href)
     const searchParams = new URLSearchParams(url.search)
     const id = searchParams.get("id")
+    const [data, setData] = useState()
 
 
     useEffect(() => {
         const method = "POST"
         const type = "getGroupEvents"
         const payload = { sessionId: getCookie("sessionId"), groupId: parseInt(id) }
-        fetchData(method, type, payload).then((res) => {
+        fetchData(method, type, payload).then((data) => {
             console.log("EventList",payload)
-            console.log(res)
+            setData(data)
         }
         )
     }, [id])
-
+    /* if (data.length === 0) {
+        return (
+            <div className="event-list">
+                <div className="event-list-header">
+                    <h1>Events</h1>
+                </div>
+                <div className="event-list-body">
+                    <div className="event-list-item">
+                        <div className="event-list-item-header">
+                            <h2>No events</h2>
+                        </div>
+                        <div className="event-list-item-body">
+                            <p>There are no events in this group</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="event-list-footer">
+                    <button onClick={clearBox}>Close</button>
+                </div>
+            </div>
+        )
+    } else {
+        console.log(data) */
     return (
-        <>
-        </>
+        <div className="event-list">
+        <div className="event-list-header">
+          <h1>Events</h1>
+        </div>
+        <div className="event-list-body">
+            {console.log(data,"data")}
+          {data.map((event, index) => (
+            <div className="event-list-item" key={index}>
+              <div className="event-list-item-header">
+                <h2>{event.title}</h2>
+              </div>
+              <div className="event-list-item-body">
+                <p>{event.content}</p>
+              </div>
+              <div className="event-list-item-footer">
+                <p>{event.date}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="event-list-footer">
+          <button onClick={clearBox}>Close</button>
+        </div>
+      </div>
     )
 }
 
