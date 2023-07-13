a3b50321358325b67d7c972f3a589d9182a568ce
Mainm0e <mainm0e1996@gmail.com>
2023-07-13 18:35:43 +0300

    is not dont yet
    continue later

--- frontend/src/Common/ChatBox/ChatRoom/ChatRoom.jsx
+++ frontend/src/Common/ChatBox/ChatRoom/ChatRoom.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./ChatRoom.css";
 import { dummyMessages } from "../DummyData";
 import { WebSocketContext } from "../../../WebSocketContext/websocketcontext";
-import { getUserId } from "../../../tools/cookie";
+import { getCookie, getUserId } from "../../../tools/cookie";
 
 const ChatRoom = (props) => {
   const sender = getUserId("userId");
@@ -10,58 +10,31 @@ const ChatRoom = (props) => {
   const [isClosed, setIsClosed] = useState(false);
   const socket = useContext(WebSocketContext);
   const [messageInput, setMessageInput] = useState("");
-
-
-  // get chat content from server
-  socket.onmessage = (event) => {
-    const message = JSON.parse(event.data);
-    console.log("Message received: ", message);
-    if (message.message === "typing") {
-      console.log("typing");
-    } else {
-      console.log("message");
-    }
-  };
+  const [chatHistory, setChatHistory] = useState([]);
+  const [newChatContent , setNewChatContent] = useState(null); // store new chat content from server
 
   const getChatContent = () => {
-    const message = {
-      sender: sender,
-      receiver: parseInt(receiver.id),
-      message: "chatHistoryRequest",
+    const payload = {
+      sessionID: getCookie("sessionId"),
+      chatType: "private",
+      clientID: getUserId("userId"),
+      targetID: receiver.id,
     };
-    socket.send(JSON.stringify(message)); // Send the message as a string
-    console.log("Message sent: ", message);
-  }
-
-  // send typing event to server when user is typing
-  const typingMessage = (e) => {
-    if (receiver.status === "online") {
-    const message = {
-      sender: getUserId("userId"),
-      receiver: parseInt(receiver.id),
-      message: "typing",
+    const chatHistoryRequest = {
+      type: "chatHistoryRequest",
+      payload: payload,
     };
-    /* socket.send(JSON.stringify(message)); // Send the message as a string */
-    console.log("Message sent: ", message);
-    }
-    setMessageInput(e)
+    socket.send(JSON.stringify(chatHistoryRequest)); // Send the message as a string
   };
 
-  // send message to server when user press enter
-  const sendMessage = () => {
-    if (messageInput.trim() !== "") {
-      const message = {
-        sender: getUserId("userId"),
-        receiver: receiver.id,
-        message: messageInput,
-      };
-      socket.send(JSON.stringify(message)); // Send the message as a string
-      setMessageInput(""); // Clear the input field
-    }
-  };
-  const chatContent = dummyMessages.map((message) => {
-    const isSender = message.sender === sender;
-    const isReceiver = message.sender === receiver.id;
+  // get chat content from server
+  useEffect(() => {
+    getChatContent();
+  }, []);
+
+  const chatContent = chatHistory.map((message) => {
+    const isSender = message.senderId === sender;
+    const isReceiver = message.receiverId === sender;
     if (!isSender && !isReceiver) {
       return null;
     }
@@ -72,10 +45,45 @@ const ChatRoom = (props) => {
         }-message`}
         key={message.id}
       >
-        <div className="chat-message">{message.message}</div>
+        <div className="chat-message">{message.messageContent}</div>
       </div>
     );
   });
+
+  // send typing event to server when user is typing
+  const typingMessage = (e) => {
+    if (receiver.status === "online") {
+      const message = {
+        sender: getUserId("userId"),
+        receiver: parseInt(receiver.id),
+        message: "typing",
+      };
+      /* socket.send(JSON.stringify(message)); // Send the message as a string */
+      /*   console.log("Message sent: ", message); */
+    }
+    setMessageInput(e);
+  };
+
+  // send message to server when user press enter
+  const sendMessage = () => {
+    if (messageInput.trim() !== "") {
+      const message = {
+        sessionID: getCookie("sessionId"),
+        senderID: getUserId("userId"),
+        receiverID: parseInt(receiver.id),
+        message: messageInput,
+        timeStamp: "",
+      };
+      const privateMessageEvent = {
+        type: "privateMsg",
+        payload: message,
+      };
+
+      socket.send(JSON.stringify(privateMessageEvent)); // Send the message as a string
+      setMessageInput(""); // Clear the input field
+    }
+  };
+
 
   const handleUserClick = () => {
     setIsClosed(true);
@@ -85,6 +93,23 @@ const ChatRoom = (props) => {
   if (isClosed) {
     return null; // Return null if the ChatRoom is closed
   }
+  socket.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+    if (message.type === "chatHistory") {
+      setChatHistory(message.payload.chatHistory);
+    }
+    if (message.type === "PrivateMsg") {
+     const newMessage = addNewMessage("receiver",message.payload);
+     
+
+
+      document.getElementsByClassName("chat-messages")[0].scrollTop =
+      document.getElementsByClassName("chat-messages")[0].scrollHeight;
+    } else if (message.type !== "chatHistory") {
+      console.log("message", message);
+    }
+  };
+
 
   return (
     <div className="chat-room">
@@ -98,7 +123,10 @@ const ChatRoom = (props) => {
         </span>
       </div>
       <div className="chat-room-content">
-        <div className="chat-messages">{chatContent}</div>
+        <div className="chat-messages">
+          {chatContent}
+
+          </div>
       </div>
       <div className="chat-room-input">
         <input
@@ -118,3 +146,11 @@ const ChatRoom = (props) => {
 };
 
 export default ChatRoom;
+
+const addNewMessage = (who,message) => {
+    return (
+      <div className={who+"-message"}>
+        <div className="chat-message">{message.messageContent}</div>
+      </div>
+    );
+}
--- frontend/src/WebSocketContext/websocketcontext.js
+++ frontend/src/WebSocketContext/websocketcontext.js
@@ -4,10 +4,12 @@ import React, { createContext, useEffect, useState } from 'react';
 export const WebSocketContext = createContext(null);
 
 // Create a provider component that will wrap other components and provide them with the websocket connection
-export const WebSocketProvider = ({ children }) => {
+export const WebSocketProvider = ({ children, isLoggedIn }) => {
   const [socket, setSocket] = useState(null);
-
   useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
     // On component mount, try to connect to the websocket server
     const ws = new WebSocket('ws://localhost:8080/ws');
 
@@ -26,7 +28,7 @@ export const WebSocketProvider = ({ children }) => {
         ws.close();
       }
     };
-  }, []);
+  }, [isLoggedIn]);
 
   return (
     // Provide the websocket connection to child components
