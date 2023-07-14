c6941aa17e642cf2d4af0c5e7b8184b34e32f9d1
mainm0e <mainm0e1996@gmail.com>
2023-07-14 19:30:41 +0300

    update chatlist

--- frontend/src/Common/ChatBox/ChatList/ChatList.jsx
+++ frontend/src/Common/ChatBox/ChatList/ChatList.jsx
@@ -1,38 +1,60 @@
-import { useEffect } from 'react';
-import {dummyUsers} from '../DummyData';
-import './ChatList.css';
-import { getCookie, getUserId } from '../../../tools/cookie';
-import { fetchData } from '../../../tools/fetchData';
-import { useState } from 'react';
+import { useEffect } from "react";
+import { dummyUsers } from "../DummyData";
+import "./ChatList.css";
+import { getCookie, getUserId } from "../../../tools/cookie";
+import { fetchData } from "../../../tools/fetchData";
+import { useState } from "react";
 const ChatList = ({ onUserSelection }) => {
   const handleUserClick = (selectedUser) => {
     // Pass the selected user data to the parent component
     onUserSelection(selectedUser);
   };
   const [contactList, setContactList] = useState([]);
-
+  const [memberlist, setMemberlist] = useState(null);
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+  const state = searchParams.get("id");
+  
   useEffect(() => {
     const fetchDataAsync = async () => {
       const users = await getContactList();
       setContactList(users);
     };
-
+  
     fetchDataAsync();
+  
+    if (url.pathname === "/group" && state !== null) {
+      const method = "POST";
+      const type = "exploreGroups";
+      const payload = {
+        sessionId: getCookie("sessionId"),
+        userId: getUserId("userId"),
+      };
+      fetchData(method, type, payload).then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].groupId === parseInt(state)) {
+            console.log(data[i].members, "fine memberlist");
+            setMemberlist(data[i].members);
+          }
+        }
+      });
+    }
   }, []);
-
+  
+  
   return (
-    <div className='chat-list'>
+    <div className="chat-list">
       {contactList.map((user) => (
         <div
-          className='chat-list-item'
+          className="chat-list-item"
           key={user.userId}
           onClick={() => handleUserClick(user)}
         >
-          <div className='chat-list-item-avatar'>
+          <div className="chat-list-item-avatar">
             <img src={user.avatar} alt={user.firstName} />
           </div>
-          <div className='chat-list-item-content'>
-            <div className='chat-list-item-content-name'>{user.firstName}</div>
+          <div className="chat-list-item-content">
+            <div className="chat-list-item-content-name">{user.firstName}</div>
           </div>
         </div>
       ))}
@@ -43,17 +65,17 @@ const ChatList = ({ onUserSelection }) => {
 export default ChatList;
 
 const getContactList = async () => {
-  const method = 'POST';
-  const type = 'profileList';
+  const method = "POST";
+  const type = "profileList";
   const payload = {
-    sessionId: getCookie('sessionId'),
-    userId: getUserId('userId'),
-    request: 'followers',
+    sessionId: getCookie("sessionId"),
+    userId: getUserId("userId"),
+    request: "followers",
   };
   const payload2 = {
-    sessionId: getCookie('sessionId'),
-    userId: getUserId('userId'),
-    request: 'followings',
+    sessionId: getCookie("sessionId"),
+    userId: getUserId("userId"),
+    request: "followings",
   };
 
   // Fetch both sets of data concurrently
@@ -71,7 +93,9 @@ const getContactList = async () => {
   }
   if (followingsData !== null) {
     followingsData.forEach((user) => {
-      const existingUser = contactList.find((contact) => contact.userId === user.userId);
+      const existingUser = contactList.find(
+        (contact) => contact.userId === user.userId
+      );
       if (!existingUser) {
         contactList.push(user);
       }
