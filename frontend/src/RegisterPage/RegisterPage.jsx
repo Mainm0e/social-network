82839575a0e482490e28eda6411e24e0e80fd08c
salam <salamfoon@gmail.com>
2023-07-17 17:21:12 +0300

    register page logic changes

--- frontend/src/RegisterPage/RegisterPage.jsx
+++ frontend/src/RegisterPage/RegisterPage.jsx
@@ -26,10 +26,10 @@ function RegisterPage() {
 
   // handleOptionChange function
   // This function is used to change the form state
-  // const handleOptionChange = (event) => {
-  //   const selectedOption = event.target.value;
-  //   setSelectedOption(selectedOption);
-  // };
+  const handleOptionChange = (event) => {
+    const selectedOption = event.target.value;
+    setSelectedOption(selectedOption);
+  };
 
   // handleInfoChange function
   // this function get the values from Info1 and Info2 components
@@ -61,14 +61,50 @@ function RegisterPage() {
 
   };
 
+  const [setStep] = useState(1);
+
   // button to navigate forward through info pages
   const handleNext = () => {
-    if (selectedOption === "info1") {
-      setSelectedOption("info2");
-    } else if (selectedOption === "info2") {
-      setSelectedOption("info3");
+    let emptyFields = [];
+    if (selectedOption === 'info1') {
+      // Check if required fields in Info1 are filled
+      if (!firstName) emptyFields.push('firstName');
+      if (!lastName) emptyFields.push('lastName');
+      if (!birthdate) emptyFields.push('birthdate');
+    } else if (selectedOption === 'info2') {
+      // Check if required fields in Info2 are filled
+      if (!email) emptyFields.push('email');
+      if (!password) emptyFields.push('password');
+      if (!confirmPassword) emptyFields.push('confirmPassword');
+    } else if (selectedOption === 'info3') {
+      // Check if required fields in Info3 are filled
+      if (!nickName) emptyFields.push('nickName');
+      if (!aboutme) emptyFields.push('aboutme');
+    }
+
+    if (emptyFields.length === 0) {
+      // All required fields are filled, proceed to the next step
+      if (selectedOption === 'info1') setSelectedOption('info2');
+      else if (selectedOption === 'info2') setSelectedOption('info3');
+      else if (selectedOption === 'info3') {
+        // If on info3, move to the next step (you can trigger the registration here)
+        setStep((prevStep) => prevStep + 1);
+      }
+    } else {
+      // Some required fields are empty, add the shake animation to the empty fields
+      emptyFields.forEach((field) => {
+        document.querySelector(`[name=${field}]`).classList.add('shake');
+      });
+
+      // Remove the shake animation after a short delay
+      setTimeout(() => {
+        emptyFields.forEach((field) => {
+          document.querySelector(`[name=${field}]`).classList.remove('shake');
+        });
+      }, 500);
     }
   };
+  
 
   // handleRegister function
   const [alertTitle, setAlertTitle] = useState("");
